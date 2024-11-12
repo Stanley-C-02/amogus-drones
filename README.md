afaa2a11b93734ca899ca414c86c48c1d8af1347
Stanley Chow <51177611+Stanley-C-02@users.noreply.github.com>
2024-11-12 16:50:36 -0500

    fix with Java Project using JavaSE-1.8

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# amogus-drones
+# amogus_drones
 A simulated drone parcel delivery system using MDE, created with itemisCREATE and Java.
---
+++ src/amogus_drones/AmogusUI.java
@@ -0,0 +1,52 @@
+package amogus_drones;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AmogusUI {
+//	public static JTextPane door_state, door_lock, door_mode, door_timer;
+
+//   @SuppressWarnings("deprecation")
+	public static void main (String[] args) {
+		JFrame window = new JFrame("AMOGUS UI - Test");
+		window.setSize(1000, 1000);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		window.setLayout(null);
+		window.setVisible(true);
+		
+//		JFrame doorFrame = new JFrame ("SmartDoorCtrl.ysc - USER INTERFACE");
+//		doorFrame.setSize(2000,2000);
+//		doorFrame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+//			
+//		SmartDoorPanel door = new SmartDoorPanel();
+//		SmartDoorControl controls = new SmartDoorControl(door);
+//			
+//		JPanel panel = new JPanel();
+//		door_state = new JTextPane();
+//		door_lock = new JTextPane();
+//		door_mode = new JTextPane();
+//		door_timer = new JTextPane();
+//			  
+//		door_state.setBackground(Color.white);
+//		door_lock.setBackground(Color.white);
+//		door_mode.setBackground(Color.white);
+//		door_timer.setBackground(Color.white);
+//			  
+//		panel.setBackground (Color.black);
+//		panel.setLayout (new BoxLayout(panel, BoxLayout.Y_AXIS));
+//		panel.add (Box.createRigidArea (new Dimension (0, 50)));
+//		panel.add (door);
+//		panel.add(door_state);
+//		panel.add(door_lock);
+//		panel.add(door_mode);
+//		panel.add(door_timer);
+//		panel.add (Box.createRigidArea (new Dimension (0, 100)));
+//		panel.add (controls);
+//		panel.add (Box.createRigidArea (new Dimension (0, 100)));
+//			  
+//		doorFrame.getContentPane().add(panel);
+//		doorFrame.pack();
+//		doorFrame.show();
+	}
+}
