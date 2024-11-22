89029420dc1b954deb2282abc010c25f028e6fd0
Stanley Chow <51177611+Stanley-C-02@users.noreply.github.com>
2024-11-21 22:15:26 -0500

    refactor HousePanel into one class

--- src/HousesPanel.java
+++ src/HousesPanel.java
@@ -14,19 +14,54 @@ public class HousesPanel extends JPanel {
         setBorder(BorderFactory.createTitledBorder("House Order Status"));
         setLayout(new GridLayout(1, houses.length, 0, 0));
         
-        this.panels = new HousePanel[houses.length];
+        panels = new HousePanel[houses.length];
 
         for (int i = 0; i < houses.length; i++) {
         	HousePanel panel = new HousePanel(houses[i]);
         	panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         	add(panel);
-        	this.panels[i] = panel;
+        	panels[i] = panel;
         }
 	}
 	
 	public void readStatechartData() {
-		for (HousePanel p : this.panels) {
+		for (HousePanel p : panels) {
 			p.readStatechartData();
+		}
+	}
+	
+	class HousePanel extends JPanel {
+		House house;
+		JLabel name, status, location;
+		
+		public HousePanel(House house) {
+			this.house = house;
+			
+	        setLayout(new GridLayout(3, 1, 0, 0));
+	        
+	        name = new JLabel();
+	        status = new JLabel();
+	        location = new JLabel();
+	        
+	        readStatechartData();
+	        
+	        add(name);
+	        add(status);
+	        add(location);
+		}
+		
+		public void readStatechartData() {
+			name.setText("House Name: " + house.getName());
+			
+			String orderStatus;
+			if (house.getPackage_id() == -1) {			
+				orderStatus = "None (-1)";
+			} else {
+				orderStatus = "Ordered #" + house.getPackage_id();
+			}
+			status.setText("Order Status: " + orderStatus);
+			
+			location.setText("Location: x" + house.getX() + " y" + house.getY());
 		}
 	}
 }
