3654e1822e6b4a592ecb8a5fa514a4c325464e86
Stanley Chow <51177611+Stanley-C-02@users.noreply.github.com>
2024-11-26 09:15:14 -0500

    Refactor DronePanel bg coloring and allow buttons to show hover

--- src/DronesPanel.java
+++ src/DronesPanel.java
@@ -14,7 +14,7 @@ public class DronesPanel extends JPanel {
         panels = new DronePanel[drones.length];
 
         for (int i = 0; i < drones.length; i++) {
-         	DronePanel panel = new DronePanel(drones[i]);
+         	DronePanel panel = new DronePanel(drones[i], mapPanel);
          	add(panel);
          	panels[i] = panel;
         }
@@ -28,147 +28,155 @@ public class DronesPanel extends JPanel {
     		p.readStatechartData();
     	}
     }
+}
 
-    class DronePanel extends JPanel {
-    	Amadrone drone;
-    	JLabel name, location, status, battery, motorType, motorDetails, motorPayload, range;
-    	JButton startDrone, stopDrone;
-    	JPanel statusPanel, motorPanel;
-    	
-    	public DronePanel(Amadrone drone) {
-    		this.drone = drone;
-    		
-    		setBackground(Color.WHITE);
-    		
-            setLayout(new GridLayout(5, 2, 0, 0));
-            
-            name = new JLabel();
-            location = new JLabel();
-            status = new JLabel();
-            battery = new JLabel();
-            motorType = new JLabel();
-            motorDetails = new JLabel();
-            motorPayload = new JLabel();
-            range = new JLabel();
-            
-            startDrone = new JButton("On");
-            stopDrone = new JButton("Off");
-
-            statusPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-            statusPanel.setBackground(Color.WHITE);
-            motorPanel = new JPanel(new GridLayout(3, 1));
-            motorPanel.setBackground(Color.WHITE);
-    		
-            readStatechartData();
-
-            // Drone name 
-            add(name);
-            add(location);
-            
-            //Drone Status
-            add(new JLabel("Status: "));
-            statusPanel.add(startDrone);
-            statusPanel.add(stopDrone);
-            statusPanel.add(status);
-            add(statusPanel);
-
-            // Battery details
-            add(new JLabel("Battery (Current/Max/%): "));
-            add(battery);
-
-            // Motor section
-            add(new JLabel("Motor:"));
-            motorPanel.add(motorType);
-            motorPanel.add(motorDetails);
-            motorPanel.add(motorPayload);
-            add(motorPanel);
-
-            // Flight Range
-            add(new JLabel("Flight Range (metres): "));
-            add(range);
-            
-            
-            // Button Listeners
-            startDrone.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    drone.raiseOn();
-                    mapPanel.setSelectedDroneId(drone.getId());
-                    readStatechartData();
-                }
-            });
-
-            stopDrone.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    drone.raiseOff();
-                    mapPanel.setSelectedDroneId(drone.getId());
-                    readStatechartData();
-                }
-            });
-            
-            
-            addMouseListener(new DronePanelMouseListener());
-    	}
-    	
-    	public void readStatechartData() {
-    		name.setText("Drone #" + drone.getId() + ": " + drone.getName());
-    		
-    		location.setText(String.format("Location: x%.2f y%.2f", drone.getX(), drone.getY()));
-    		
-    		status.setText(drone.getStatus());
-    		
-    		startDrone.setBackground(drone.isStateActive(Amadrone.State.MAIN_REGION_ON) ? Color.GREEN : Color.LIGHT_GRAY);
-    		stopDrone.setBackground(drone.isStateActive(Amadrone.State.MAIN_REGION_OFF) ? Color.PINK : Color.LIGHT_GRAY);
-
-            battery.setText(drone.getBattery().getAvailable() + "Wh / " + drone.getBattery().getMaxCapacity() + "Wh / " + drone.getBattery().getCharge() + "%");
-            
-            motorType.setText("Type #" + drone.getMotor().getId() + ": " + drone.getMotor().getName());
-            motorDetails.setText(String.format("Power: %.2f W, Speed: %.2f m/s", drone.getMotor().getPower(), drone.getMotor().getSpeed()));
-            motorPayload.setText("Payload: " + drone.getMotor().getMax_payload() + " grams");
-            
-//            TODO Integrate this into drone statechart???
-            final int flightRange = (int) ((drone.getBattery().getAvailable() / drone.getMotor().getPower()) * drone.getMotor().getSpeed() * 3600);
-            range.setText(String.valueOf(flightRange));
-    	}
+class DronePanel extends JPanel {
+	private static Color colorBgDefault = Color.WHITE;
+	private static Color colorBgFocus = Color.LIGHT_GRAY;
+	
+	Amadrone drone;
+	MapPanel mapPanel;
+	JLabel name, location, status, battery, motorType, motorDetails, motorPayload, range;
+	JButton startDrone, stopDrone;
+	JPanel statusPanel, motorPanel;
+	
+	public DronePanel(Amadrone drone, MapPanel mapPanel) {
+		this.drone = drone;
+		this.mapPanel = mapPanel;
+		
+        setLayout(new GridLayout(5, 2, 0, 0));
         
-        private class DronePanelMouseListener implements MouseListener {
+        name = new JLabel();
+        location = new JLabel();
+        status = new JLabel();
+        battery = new JLabel();
+        motorType = new JLabel();
+        motorDetails = new JLabel();
+        motorPayload = new JLabel();
+        range = new JLabel();
+        
+        startDrone = new JButton("On");
+        stopDrone = new JButton("Off");
 
-    		@Override
-    		public void mouseClicked(MouseEvent arg0) {
-    			// TODO Auto-generated method stub
-    			
-    		}
+        startDrone.addMouseListener(new DronePanelMouseListener());
+        stopDrone.addMouseListener(new DronePanelMouseListener());
 
-    		@Override
-    		public void mouseEntered(MouseEvent e) {
-    			DronePanel p = (DronePanel) e.getSource();
-    			p.setBackground(Color.LIGHT_GRAY);
-    			p.statusPanel.setBackground(Color.LIGHT_GRAY);
-    			p.motorPanel.setBackground(Color.LIGHT_GRAY);
-                mapPanel.setSelectedDroneId(p.drone.getId());
-    		}
-    		
-    		@Override
-    		public void mouseExited(MouseEvent e) {
-    			DronePanel p = (DronePanel) e.getSource();
-    			p.setBackground(Color.WHITE);
-    			p.statusPanel.setBackground(Color.WHITE);
-    			p.motorPanel.setBackground(Color.WHITE);
-                mapPanel.clearSelectedDroneId();
-    		}
+        statusPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        motorPanel = new JPanel(new GridLayout(3, 1));
+		
+        readStatechartData();
 
-    		@Override
-    		public void mousePressed(MouseEvent arg0) {
-    			// TODO Auto-generated method stub
-    			
-    		}
+        // Drone name 
+        add(name);
+        add(location);
+        
+        //Drone Status
+        add(new JLabel("Status: "));
+        statusPanel.add(startDrone);
+        statusPanel.add(stopDrone);
+        statusPanel.add(status);
+        add(statusPanel);
 
-    		@Override
-    		public void mouseReleased(MouseEvent arg0) {
-    			// TODO Auto-generated method stub
-    			
-    		}
-        }
+        // Battery details
+        add(new JLabel("Battery (Current/Max/%): "));
+        add(battery);
+
+        // Motor section
+        add(new JLabel("Motor:"));
+        motorPanel.add(motorType);
+        motorPanel.add(motorDetails);
+        motorPanel.add(motorPayload);
+        add(motorPanel);
+
+        // Flight Range
+        add(new JLabel("Flight Range (metres): "));
+        add(range);
+        
+        
+        // Button Listeners
+        startDrone.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drone.raiseOn();
+                mapPanel.setSelectedDroneId(drone.getId());
+                readStatechartData();
+            }
+        });
+
+        stopDrone.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drone.raiseOff();
+                mapPanel.setSelectedDroneId(drone.getId());
+                readStatechartData();
+            }
+        });
+        
+		setBgColor(colorBgDefault);
+        addMouseListener(new DronePanelMouseListener());
+	}
+	
+	public void readStatechartData() {
+		name.setText("Drone #" + drone.getId() + ": " + drone.getName());
+		
+		location.setText(String.format("Location: x%.2f y%.2f", drone.getX(), drone.getY()));
+		
+		status.setText(drone.getStatus());
+
+		startDrone.setBackground(drone.isStateActive(Amadrone.State.MAIN_REGION_ON) ? Color.GREEN : Color.DARK_GRAY);
+		stopDrone.setBackground(drone.isStateActive(Amadrone.State.MAIN_REGION_OFF) ? Color.PINK : Color.DARK_GRAY);
+		startDrone.setForeground(drone.isStateActive(Amadrone.State.MAIN_REGION_ON) ? Color.BLACK : Color.WHITE);
+		stopDrone.setForeground(drone.isStateActive(Amadrone.State.MAIN_REGION_OFF) ? Color.BLACK : Color.WHITE);
+
+        battery.setText(drone.getBattery().getAvailable() + "Wh / " + drone.getBattery().getMaxCapacity() + "Wh / " + drone.getBattery().getCharge() + "%");
+        
+        motorType.setText("Type #" + drone.getMotor().getId() + ": " + drone.getMotor().getName());
+        motorDetails.setText(String.format("Power: %.2f W, Speed: %.2f m/s", drone.getMotor().getPower(), drone.getMotor().getSpeed()));
+        motorPayload.setText("Payload: " + drone.getMotor().getMax_payload() + " grams");
+        
+//        TODO Integrate this into drone statechart???
+        final int flightRange = (int) ((drone.getBattery().getAvailable() / drone.getMotor().getPower()) * drone.getMotor().getSpeed() * 3600);
+        range.setText(String.valueOf(flightRange));
+	}
+	
+	void setBgColor(Color color) {
+		this.setBackground(color);
+		this.statusPanel.setBackground(color);
+		this.motorPanel.setBackground(color);
+	}
+    
+    private class DronePanelMouseListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+//			DronePanel p = (DronePanel) e.getSource();
+			setBgColor(colorBgFocus);
+            mapPanel.setSelectedDroneId(drone.getId());
+		}
+		
+		@Override
+		public void mouseExited(MouseEvent e) {
+//			DronePanel p = (DronePanel) e.getSource();
+			setBgColor(colorBgDefault);
+            mapPanel.clearSelectedDroneId();
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
     }
 }
