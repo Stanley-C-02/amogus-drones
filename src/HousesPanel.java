9b62835e3faedb048c8313194c7e21b55f257564
Stanley Chow <51177611+ForTheWindFTW@users.noreply.github.com>
2024-11-28 17:15:18 -0500

    More colours

--- src/HousesPanel.java
+++ src/HousesPanel.java
@@ -22,7 +22,7 @@ public class HousesPanel extends JPanel {
         panels = new HousePanel[houses.length];
 
         for (int i = 0; i < houses.length; i++) {
-        	HousePanel panel = new HousePanel(houses[i]);
+        	HousePanel panel = new HousePanel(mapPanel, houses[i]);
         	panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         	add(panel);
         	panels[i] = panel;
@@ -34,69 +34,83 @@ public class HousesPanel extends JPanel {
 			p.readStatechartData();
 		}
 	}
+}
+
+class HousePanel extends JPanel {
+	private static Color colorBgDefault = Color.WHITE;
+	private static Color colorBgFocus = Color.LIGHT_GRAY;
+	private static Color colorBgMoving = new Color(255, 238, 137);
 	
-	class HousePanel extends JPanel {
-		House house;
-		JLabel name, status, location;
+	private MapPanel mapPanel;
+	House house;
+	JLabel name, status, location;
+	
+	public HousePanel(MapPanel mapPanel, House house) {
+		this.house = house;
+		this.mapPanel = mapPanel;
 		
-		public HousePanel(House house) {
-			this.house = house;
-			
-			setBackground(Color.WHITE);
-			
-	        setLayout(new GridLayout(3, 1, 0, 0));
-	        
-	        name = new JLabel();
-	        status = new JLabel();
-	        location = new JLabel();
-	        
-	        readStatechartData();
-	        
-	        add(name);
-	        add(status);
-	        add(location);
-	        
-	        addMouseListener(new HousePanelMouseListener());
-		}
+		setBackground(Color.WHITE);
 		
-		public void readStatechartData() {
-			name.setText("House Name: " + house.getName());
-			
-			String orderStatus;
-			if (house.getPackage() == null) {
-				orderStatus = "None";
-			} else {
-				orderStatus = "#" + house.getPackage().getId() + ": " + house.getPackage().getName() + " (" + house.getPackage().getWeight() + "g)";
-			}
-			status.setText("Order Status: " + orderStatus);
-			
-			location.setText("Location: x" + house.getX() + " y" + house.getY());
-		}
-
-        private class HousePanelMouseListener implements MouseListener {
-
-    		@Override
-    		public void mouseClicked(MouseEvent arg0) {}
-
-    		@Override
-    		public void mouseEntered(MouseEvent e) {
-    			HousePanel p = (HousePanel) e.getSource();
-    			p.setBackground(Color.LIGHT_GRAY);
-                mapPanel.setSelectedHouseId(p.house.getId());
-    		}
-    		
-    		@Override
-    		public void mouseExited(MouseEvent e) {
-    			HousePanel p = (HousePanel) e.getSource();
-    			p.setBackground(Color.WHITE);
-                mapPanel.clearSelectedHouseId();
-    		}
-
-    		@Override
-    		public void mousePressed(MouseEvent arg0) {}
-
-    		@Override
-    		public void mouseReleased(MouseEvent arg0) {}
-        }
+        setLayout(new GridLayout(3, 1, 0, 0));
+        
+        name = new JLabel();
+        status = new JLabel();
+        location = new JLabel();
+        
+        readStatechartData();
+        
+        add(name);
+        add(status);
+        add(location);
+        
+        addMouseListener(new HousePanelMouseListener());
 	}
+	
+	public void readStatechartData() {
+		name.setText("House Name: " + house.getName());
+		
+		String orderStatus;
+		if (house.getPackage() == null) {
+			orderStatus = "None";
+		} else {
+			orderStatus = "#" + house.getPackage().getId() + ": " + house.getPackage().getName() + " (" + house.getPackage().getWeight() + "g)";
+		}
+		status.setText("Order Status: " + orderStatus);
+		
+		location.setText("Location: x" + house.getX() + " y" + house.getY());
+		
+		setBgDefault();
+	}
+	
+	void setBgFocus() {
+		setBackground(colorBgFocus);
+	}
+	
+	void setBgDefault() {
+		setBackground(house.getPackage() == null ? colorBgDefault : colorBgMoving);
+	}
+
+    private class HousePanelMouseListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			setBgFocus();
+            mapPanel.setSelectedHouseId(house.getId());
+		}
+		
+		@Override
+		public void mouseExited(MouseEvent e) {
+			setBgDefault();
+            mapPanel.clearSelectedHouseId();
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {}
+    }
 }
